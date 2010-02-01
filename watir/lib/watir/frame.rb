4321488495d4130b68388c15b2d6e067bbadc10a
Bret Pettichord <bret@pettichord.com>
2010-02-01 17:41:07 -0600

    Frame enhancements.   1. Now able to use browser.frame(:title, 'Title') to access frames.   2. Now able to use browser.frame(...).src to get the url of the frame.      This will even work for frames who otherwise would give an access      denied error.

--- watir/lib/watir/frame.rb
+++ watir/lib/watir/frame.rb
@@ -15,36 +15,44 @@ module Watir
         case how
         when :index
           index = i + 1
-          return this_frame if index == what
+          return @o = this_frame if index == what
         when :name
           begin
-            return this_frame if what.matches(this_frame.name)
+            return @o = this_frame if what.matches(this_frame.name)
           rescue # access denied?
           end
-        when :id
+        when :id, :src, :title
           # We assume that pages contain frames or iframes, but not both.
-          this_frame_tag = @container.document.getElementsByTagName("FRAME").item(i)
-          return this_frame if this_frame_tag and what.matches(this_frame_tag.invoke("id"))
-          this_iframe_tag = @container.document.getElementsByTagName("IFRAME").item(i)
-          return this_frame if this_iframe_tag and what.matches(this_iframe_tag.invoke("id"))
-        when :src
-          this_frame_tag = @container.document.getElementsByTagName("FRAME").item(i)
-          return this_frame if this_frame_tag and what.matches(this_frame_tag.src)
-          this_iframe_tag = @container.document.getElementsByTagName("IFRAME").item(i) 
-          return this_frame if this_iframe_tag and what.matches(this_iframe_tag.src)
+          return if tag_match? this_frame, i, what, "FRAME", how
+          return if tag_match? this_frame, i, what, "IFRAME", how
         else
           raise ArgumentError, "Argument #{how} not supported"
         end
       end
       
-      raise UnknownFrameException, "Unable to locate a frame with #{how.to_s} #{what}"
+      raise UnknownFrameException, "Unable to locate a frame with #{how} #{what}"
     end
+
+    # Returns whether the attribute of the tag for the indexed frame matches the what.
+    # If so, also sets @o and @tag.
+    def tag_match? frame, index, what, tag_name, attribute
+      tag = @container.document.getElementsByTagName(tag_name).item(index)
+      return false if tag.nil?
+      if what.matches(tag.invoke(attribute.to_s))
+        @o = frame
+        @tag = tag
+        true
+      else
+        false
+      end
+    end
+    private :tag_match?
     
     def initialize(container, how, what)
       set_container container
       @how = how
       @what = what
-      @o = locate
+      locate
       copy_test_config container
     end
     
@@ -56,5 +64,8 @@ module Watir
       @container.page_container.attach_command + ".frame(#{@how.inspect}, #{@what.inspect})"
     end
     
+    def src
+      @tag.src
+    end
   end
 end
